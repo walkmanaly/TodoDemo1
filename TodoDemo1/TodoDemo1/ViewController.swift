6db8c991d53cecf6d85eddb3305bb6c15c96c75e
Nick <walkmanaly@gmail.com>
2017-11-15 22:50:35 +0800

    添加数据，以及左滑删除功能

--- TodoDemo1/TodoDemo1/ViewController.swift
+++ TodoDemo1/TodoDemo1/ViewController.swift
@@ -19,26 +19,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello world!")
         todoItems = [
-            TodoItem(id: 1, name: "go to work", image: "image1", detail: "9:00"),
-            TodoItem(id: 2, name: "eat lunch", image: "image2", detail: "12:00"),
-            TodoItem(id: 3, name: "go home", image: "image3", detail: "18:00"),
-            TodoItem(id: 4, name: "eat dinner", image: "image4", detail: "19:00")
+            TodoItem(id: 1, name: "take care baby", image: "child-selected", detail: "9:00"),
+            TodoItem(id: 2, name: "have a phone call", image: "phone-selected", detail: "12:00"),
+            TodoItem(id: 3, name: "go shopping", image: "shopping-cart-selected", detail: "18:00"),
+            TodoItem(id: 4, name: "have a traval", image: "travel-selected", detail: "19:00")
         ]
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
 
+func setNoMoreDataLabel(_ label: UILabel,
+                        frame: CGRect,
+                        text: String,
+                        textAliment: NSTextAlignment,
+                        font: UIFont,
+                        textColor: UIColor) {
+    label.frame = frame
+    label.text = text
+    label.textAlignment = textAliment
+    label.font = font
+    label.textColor = textColor
+}
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if todoItems.count > 0 {
             return todoItems.count
         } else {
+            let messageLabel = UILabel()
+            setNoMoreDataLabel(messageLabel, frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height), text: "no more data", textAliment: .center, font: UIFont(name: "Palatino-Italic", size: 20)!, textColor: .black)
+            tableView.backgroundView = messageLabel
+            tableView.separatorStyle = UITableViewCellSeparatorStyle.none
             return 0
         }
     }
@@ -46,7 +57,10 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let ID: String = "TodoItemCell"
         let cell = tableView .dequeueReusableCell(withIdentifier: ID, for: indexPath)
-        
+        let item = todoItems[indexPath.row]
+        cell.imageView?.image = UIImage(named: item.image)
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = item.detail
         return cell
     }
     
@@ -54,7 +68,26 @@ extension ViewController: UITableViewDataSource {
 }
 
 extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 64
+    }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected")
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            todoItems.remove(at: indexPath.row)
+            
+            tableView.reloadData()
+        }
+    }
 }
 
 
